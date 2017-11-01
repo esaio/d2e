e741a6d80356634f330589431eb909d7c6e25b67
fukayatsu <fukayatsu@gmail.com>
2017-11-01 13:44:43 +0900

    [from now] 2017/11/01 13:44:43
    diff --git a/app.rb b/app.rb
    index 7cec9fd..c67f999 100644
    --- a/app.rb
    +++ b/app.rb
    @@ -3,10 +3,9 @@ require 'optparse'
     require 'yaml'
     require_relative './importer'
    
    -params = ARGV.getopts('', 'run', 'start:0')
     config = YAML.load_file('config.yml')
    -config = YAML.load_file('dirs.yml')
    -config = YAML.load_file('users.yml')
    -
    +users  = YAML.load_file('users.yml')
     importer = Importer.new(client, config: config, users: users)
    +
    +params = ARGV.getopts('', 'run', 'start:0')
     importer.import(dry_run: !params['run'], start_index: params['start'].to_i)

--- app.rb
+++ app.rb
@@ -3,10 +3,9 @@ require 'optparse'
 require 'yaml'
 require_relative './importer'
 
-params = ARGV.getopts('', 'run', 'start:0')
 config = YAML.load_file('config.yml')
-config = YAML.load_file('dirs.yml')
-config = YAML.load_file('users.yml')
-
+users  = YAML.load_file('users.yml')
 importer = Importer.new(client, config: config, users: users)
+
+params = ARGV.getopts('', 'run', 'start:0')
 importer.import(dry_run: !params['run'], start_index: params['start'].to_i)
